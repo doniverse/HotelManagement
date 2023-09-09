425d3c7d3d6cf33900b01f0574c2e6f0080c08dc
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2023-09-09 12:36:40 +0300

    login page added

--- src/Main.java
+++ src/Main.java
@@ -13,43 +13,79 @@ class HomePage{
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
+
+
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
-
-        JFrame frame = new JFrame("Multi-Page Screen Example");
+        com.formdev.flatlaf.themes.FlatMacLightLaf.install();
+        JFrame frame = new JFrame("Hotel Management");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1100, 700);
+        frame.setResizable(false);
+
         JPanel container = new JPanel(new CardLayout());
 
-        JPanel page1 = new JPanel();
+        // LogInPage
+        JPanel logInPage = new JPanel();
+        logInPage.setLayout(null);
+
         JButton nextPageButton = new JButton("Next Page");
+        nextPageButton.setBounds(500, 400, 100, 30);
+
+        JLabel logInTitle = new JLabel("Log In");
+        logInTitle.setBounds(480, 100, 200, 100);
+        logInTitle.setFont(new Font(logInTitle.getFont().getName(), Font.PLAIN, 48));
+
+        JLabel usernameLabel = new JLabel("Username: ");
+        usernameLabel.setBounds(440, 150, 200, 100);
+        JTextField usernameInput = new JTextField();
+        usernameInput.setBounds(440, 215, 220,25);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(440, 220, 200, 100);
+        JPasswordField passwordInput = new JPasswordField();
+        passwordInput.setBounds(440, 280, 220,25);
+
+
+        JButton logInButton = new JButton("Log In");
+        logInButton.setBounds(440, 320, 220, 30);
+        JButton createAccountButton = new JButton("Create Account");
+        createAccountButton.setBounds(440, 355, 220,30);
+
         nextPageButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 CardLayout cardLayout = (CardLayout) container.getLayout();
-                cardLayout.show(container, "Page2");
+                cardLayout.show(container, "dashboard");
             }
         });
-        page1.add(nextPageButton);
-        page1.add(new JLabel("Page One here"));
 
-        JPanel page2 = new JPanel();
+        logInPage.add(nextPageButton);
+        logInPage.add(logInTitle);
+        logInPage.add(usernameLabel);
+        logInPage.add(usernameInput);
+        logInPage.add(passwordLabel);
+        logInPage.add(passwordInput);
+        logInPage.add(logInButton);
+        logInPage.add(createAccountButton);
+        logInPage.add(new JLabel("Log In page"));
+
+
+
+        JPanel dashboard = new JPanel();
         JButton prevPageButton = new JButton("Previous Page");
         prevPageButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 CardLayout cardLayout = (CardLayout) container.getLayout();
-                cardLayout.show(container, "Page1");
+                cardLayout.show(container, "logInPage");
             }
         });
-        page2.add(prevPageButton);
-        ;page2.add(new JLabel("Here is page 2"));
+        dashboard.add(prevPageButton);
+        dashboard.add(new JLabel("Dashboard Here"));
 
-        container.add(page1, "Page1");
-        container.add(page2, "Page2");
+        container.add(logInPage, "logInPage");
+        container.add(dashboard, "dashboard");
 
         frame.add(container);
         frame.setVisible(true);
