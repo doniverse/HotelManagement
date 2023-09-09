a18256dc431d8517f7fb0b51b2505ab2d036fab3
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2023-09-09 10:48:18 +0300

    multi page naviagtion added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.event.*;
+import java.awt.event.*;
 
 
 
@@ -16,6 +18,40 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        new HomePage();
+
+
+
+        JFrame frame = new JFrame("Multi-Page Screen Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        JPanel container = new JPanel(new CardLayout());
+
+        JPanel page1 = new JPanel();
+        JButton nextPageButton = new JButton("Next Page");
+        nextPageButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) container.getLayout();
+                cardLayout.show(container, "Page2");
+            }
+        });
+        page1.add(nextPageButton);
+        page1.add(new JLabel("Page One here"));
+
+        JPanel page2 = new JPanel();
+        JButton prevPageButton = new JButton("Previous Page");
+        prevPageButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cardLayout = (CardLayout) container.getLayout();
+                cardLayout.show(container, "Page1");
+            }
+        });
+        page2.add(prevPageButton);
+        ;page2.add(new JLabel("Here is page 2"));
+
+        container.add(page1, "Page1");
+        container.add(page2, "Page2");
+
+        frame.add(container);
+        frame.setVisible(true);
     }
 }
