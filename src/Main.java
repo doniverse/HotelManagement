53facd9126d2fbc84b44c3f0f584c1b6a72f2c34
Naol Fekadu Hunduma <nf.naol9@gmail.com>
2023-09-09 10:22:19 +0300

    First push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+class HomePage{
+    HomePage(){
+        JFrame mainFrame = new JFrame("Hotel Manager");
+        mainFrame.setSize(500,500);
+        mainFrame.setVisible(true);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        new HomePage();
     }
 }
